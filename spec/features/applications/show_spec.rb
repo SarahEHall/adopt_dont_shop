5fe5be8153a472f3f54d5ae6feceac7a0751c644
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-23 15:11:38 -0600

    Refactor: functionality out of _add_pets to _pet_search

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -1,108 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Application Show Page', type: :feature do
-    describe 'visuals' do
-        it 'can show attributes of the application' do
-            shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-            pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-            pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-            pet_3 = shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
-
-            application1 = Application.create!(
-                name: 'Joe Exotic',
-                street_address: '3150 Horton Rd',
-                city: 'Fort Worth',
-                state: 'TX',
-                zip_code: 76119,
-                description: 'I just really love animals',
-                status: 'Rejected'
-            )
-            application2 = Application.create!(
-                name: 'Carol Baskins',
-                street_address: '12802 Easy St',
-                city: 'Tampa',
-                state: 'FL',
-                zip_code: 33625,
-                description: 'I just really love animals more than that other guy',
-                status: 'Rejected'
-            )
-
-            pet_application_1 = PetApplication.create!(pet: pet_1, application: application1)
-            pet_application_2 = PetApplication.create!(pet: pet_2, application: application2)
-            pet_application_3 = PetApplication.create!(pet: pet_3, application: application2)
-
-            visit "/applications/#{application1.id}"
-            # save_and_open_page
-            expect(page).to have_content(application1.name)
-            expect(page).to have_content("Full address: 3150 Horton Rd, Fort Worth, TX 76119")
-            expect(page).to have_content(application1.description)
-            expect(page).to have_content(application1.status)
-
-            expect(page).to_not have_content('Carol Baskins')
-            expect(page).to_not have_content("Full address: 12802 Easy St, Tampa, fL 33625")
-            expect(page).to_not have_content('I just really love animals more than that other guy')
-
-            click_link "#{pet_1.name}"
-            expect(current_path).to eq("/pets/#{pet_1.id}")
-            expect(page).to have_content(pet_1.name)
-
-            visit "/applications/#{application2.id}"
-
-            expect(page).to have_link("#{pet_2.name}")
-            expect(page).to have_link("#{pet_3.name}")
-          end 
-        end
-
-        it 'can test for multiple pets' do
-            shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-            pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-            pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
-            pet_3 = shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
-
-            application1 = Application.create!(
-                name: 'Joe Exotic',
-                street_address: '3150 Horton Rd',
-                city: 'Fort Worth',
-                state: 'TX',
-                zip_code: 76119,
-                description: 'I just really love animals',
-                status: 'Rejected'
-            )
-            application2 = Application.create!(
-                name: 'Carol Baskins',
-                street_address: '12802 Easy St',
-                city: 'Tampa',
-                state: 'FL',
-                zip_code: 33625,
-                description: 'I just really love animals more than that other guy',
-                status: 'Rejected'
-            )
-            pet_application1 = PetApplication.create!(pet: pet_1, application: application1)
-            pet_application2 = PetApplication.create!(pet: pet_2, application: application2)
-            pet_application3 = PetApplication.create!(pet: pet_3, application: application2)
-
-            visit "/applications/#{application2.id}"
-            # save_and_open_page
-            expect(page).to have_content(application2.name)
-            expect(page).to have_content("Full address: #{application2.street_address}, #{application2.city}, #{application2.state} #{application2.zip_code}")
-            expect(page).to have_content(application2.description)
-            expect(page).to have_content(application2.status)
-
-            expect(page).to_not have_content(application1.name)
-
-            click_link "#{pet_2.name}"
-            expect(current_path).to eq("/pets/#{pet_2.id}")
-            expect(page).to have_content(pet_2.name)
-
-            visit "/applications/#{application2.id}"
-
-            click_link "#{pet_3.name}"
-            expect(current_path).to eq("/pets/#{pet_3.id}")
-            expect(page).to have_content(pet_3.name)
-        end
-
-  describe 'Searching for Pets for an Application' do
-    it 'has a search section that shows up on applications that are in progress' do
+  describe 'visuals' do
+    it 'can show attributes of the application' do
       shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
       pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
       pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
@@ -117,15 +17,115 @@ RSpec.describe 'Application Show Page', type: :feature do
           description: 'I just really love animals',
           status: 'Rejected'
       )
+      application2 = Application.create!(
+          name: 'Carol Baskins',
+          street_address: '12802 Easy St',
+          city: 'Tampa',
+          state: 'FL',
+          zip_code: 33625,
+          description: 'I just really love animals more than that other guy',
+          status: 'Rejected'
+      )
+
+      pet_application_1 = PetApplication.create!(pet: pet_1, application: application1)
+      pet_application_2 = PetApplication.create!(pet: pet_2, application: application2)
+      pet_application_3 = PetApplication.create!(pet: pet_3, application: application2)
+
+      visit "/applications/#{application1.id}"
+      # save_and_open_page
+      expect(page).to have_content(application1.name)
+      expect(page).to have_content("Full address: 3150 Horton Rd, Fort Worth, TX 76119")
+      expect(page).to have_content(application1.description)
+      expect(page).to have_content(application1.status)
+
+      expect(page).to_not have_content('Carol Baskins')
+      expect(page).to_not have_content("Full address: 12802 Easy St, Tampa, fL 33625")
+      expect(page).to_not have_content('I just really love animals more than that other guy')
+
+      click_link "#{pet_1.name}"
+      expect(current_path).to eq("/pets/#{pet_1.id}")
+      expect(page).to have_content(pet_1.name)
+
+      visit "/applications/#{application2.id}"
+
+      expect(page).to have_link("#{pet_2.name}")
+      expect(page).to have_link("#{pet_3.name}")
+    end 
+  end
+
+  it 'can test for multiple pets' do
+    shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+    pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+    pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    pet_3 = shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
+
+    application1 = Application.create!(
+        name: 'Joe Exotic',
+        street_address: '3150 Horton Rd',
+        city: 'Fort Worth',
+        state: 'TX',
+        zip_code: 76119,
+        description: 'I just really love animals',
+        status: 'Rejected'
+    )
+    application2 = Application.create!(
+        name: 'Carol Baskins',
+        street_address: '12802 Easy St',
+        city: 'Tampa',
+        state: 'FL',
+        zip_code: 33625,
+        description: 'I just really love animals more than that other guy',
+        status: 'Rejected'
+    )
+    pet_application1 = PetApplication.create!(pet: pet_1, application: application1)
+    pet_application2 = PetApplication.create!(pet: pet_2, application: application2)
+    pet_application3 = PetApplication.create!(pet: pet_3, application: application2)
+
+    visit "/applications/#{application2.id}"
+    # save_and_open_page
+    expect(page).to have_content(application2.name)
+    expect(page).to have_content("Full address: #{application2.street_address}, #{application2.city}, #{application2.state} #{application2.zip_code}")
+    expect(page).to have_content(application2.description)
+    expect(page).to have_content(application2.status)
+
+    expect(page).to_not have_content(application1.name)
+
+    click_link "#{pet_2.name}"
+    expect(current_path).to eq("/pets/#{pet_2.id}")
+    expect(page).to have_content(pet_2.name)
+
+    visit "/applications/#{application2.id}"
+
+    click_link "#{pet_3.name}"
+    expect(current_path).to eq("/pets/#{pet_3.id}")
+    expect(page).to have_content(pet_3.name)
+  end
+
+  describe 'Searching for Pets for an Application' do
+    it 'has a search section that shows up on applications that are in progress' do
+      shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      pet_1 = shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+      pet_2 = shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+      pet_3 = shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
+
+      application1 = Application.create!(
+        name: 'Joe Exotic',
+        street_address: '3150 Horton Rd',
+        city: 'Fort Worth',
+        state: 'TX',
+        zip_code: 76119,
+        description: 'I just really love animals',
+        status: 'Rejected'
+      )
 
       application2 = Application.create!(
-          name: 'Spongebob',
-          street_address: '124 Conch lane',
-          city: 'Bikini Bottom',
-          state: 'Despair',
-          zip_code: 33025,
-          description: "I'm ready!",
-          status: 'In Progress'
+        name: 'Spongebob',
+        street_address: '124 Conch lane',
+        city: 'Bikini Bottom',
+        state: 'Despair',
+        zip_code: 33025,
+        description: "I'm ready!",
+        status: 'In Progress'
       )
       visit "/applications/#{application1.id}"
 
@@ -143,19 +143,19 @@ RSpec.describe 'Application Show Page', type: :feature do
       pet_3 = shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
 
       application2 = Application.create!(
-          name: 'Spongebob',
-          street_address: '124 Conch lane',
-          city: 'Bikini Bottom',
-          state: 'Despair',
-          zip_code: 33025,
-          description: "I'm ready!",
-          status: 'In Progress'
+        name: 'Spongebob',
+        street_address: '124 Conch lane',
+        city: 'Bikini Bottom',
+        state: 'Despair',
+        zip_code: 33025,
+        description: "I'm ready!",
+        status: 'In Progress'
       )
 
       visit "/applications/#{application2.id}"
 
       fill_in(:search, with: "Clawdia")
-      click_button("Submit")
+      click_button("Search")
 
 
       expect(page).to have_content("Clawdia")
@@ -176,20 +176,20 @@ RSpec.describe 'Application Show Page', type: :feature do
       pet_3 = shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 3, adoptable: false)
 
       application2 = Application.create!(
-          name: 'Spongebob',
-          street_address: '124 Conch lane',
-          city: 'Bikini Bottom',
-          state: 'Despair',
-          zip_code: 33025,
-          description: "I'm ready!",
-          status: 'In Progress'
+        name: 'Spongebob',
+        street_address: '124 Conch lane',
+        city: 'Bikini Bottom',
+        state: 'Despair',
+        zip_code: 33025,
+        description: "I'm ready!",
+        status: 'In Progress'
       )
 
       visit "/applications/#{application2.id}"
       expect(page).to have_content(application2.name)
 
       fill_in(:search, with: "Ann")
-      click_button("Submit")
+      click_button("Search")
 
       click_button "Adopt #{pet_3.name}"
 
@@ -218,7 +218,7 @@ RSpec.describe 'Application Show Page', type: :feature do
       expect(page).to have_content(application2.name)
 
       fill_in(:search, with: "Ann")
-      click_button("Submit")
+      click_button("Search")
 
       expect(page).to have_content(pet_1.name)
       expect(page).to have_content(pet_2.name)
@@ -273,27 +273,27 @@ RSpec.describe 'Application Show Page', type: :feature do
 
   describe 'wonky matches for pet search' do 
     before(:each) do 
-        shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
-        @pet_1 = shelter_1.pets.create(name: 'Annabelle', breed: 'tuxedo shorthair', age: 5, adoptable: true)
-        @pet_2 = shelter_1.pets.create(name: 'Annie', breed: 'shorthair', age: 3, adoptable: true)
-        @pet_3 = shelter_1.pets.create(name: 'Barbara Ann', breed: 'ragdoll', age: 3, adoptable: false)
-        @pet_4 = shelter_1.pets.create(name: 'Soup', breed: 'box turtle', age: 45, adoptable: false)
-        @application2 = Application.create!(
-            name: 'Spongebob',
-            street_address: '124 Conch lane',
-            city: 'Bikini Bottom',
-            state: 'Despair',
-            zip_code: 33025,
-            description: "",
-            status: 'In Progress'
-        )
+      shelter_1 = Shelter.create(name: 'Aurora shelter', city: 'Aurora, CO', foster_program: false, rank: 9)
+      @pet_1 = shelter_1.pets.create(name: 'Annabelle', breed: 'tuxedo shorthair', age: 5, adoptable: true)
+      @pet_2 = shelter_1.pets.create(name: 'Annie', breed: 'shorthair', age: 3, adoptable: true)
+      @pet_3 = shelter_1.pets.create(name: 'Barbara Ann', breed: 'ragdoll', age: 3, adoptable: false)
+      @pet_4 = shelter_1.pets.create(name: 'Soup', breed: 'box turtle', age: 45, adoptable: false)
+      @application2 = Application.create!(
+          name: 'Spongebob',
+          street_address: '124 Conch lane',
+          city: 'Bikini Bottom',
+          state: 'Despair',
+          zip_code: 33025,
+          description: "",
+          status: 'In Progress'
+      )
 
-        visit "/applications/#{@application2.id}"
+      visit "/applications/#{@application2.id}"
     end
 
     it 'can return pets whose name partially matches a search' do 
       fill_in(:search, with: "Ann")
-      click_button("Submit")
+      click_button("Search")
 
       expect(page).to have_content("Annabelle")
       expect(page).to have_content("Ann")
@@ -302,7 +302,7 @@ RSpec.describe 'Application Show Page', type: :feature do
 
       visit "/applications/#{@application2.id}"
       fill_in(:search, with: "n")
-      click_button("Submit")
+      click_button("Search")
 
       expect(page).to have_content("Annabelle")
       expect(page).to have_content("Ann")
@@ -311,22 +311,22 @@ RSpec.describe 'Application Show Page', type: :feature do
     end
     
     it 'produces results even if case is different' do 
-        fill_in(:search, with: "aNn")
-        click_button("Submit")
+      fill_in(:search, with: "aNn")
+      click_button("Search")
 
-        expect(page).to have_content("Annabelle")
-        expect(page).to have_content("Ann")
-        expect(page).to have_content("Barbara Ann")
-        expect(page).to_not have_content("Soup")
+      expect(page).to have_content("Annabelle")
+      expect(page).to have_content("Ann")
+      expect(page).to have_content("Barbara Ann")
+      expect(page).to_not have_content("Soup")
 
-        visit "/applications/#{@application2.id}"
-        fill_in(:search, with: "ANN")
-        click_button("Submit")
+      visit "/applications/#{@application2.id}"
+      fill_in(:search, with: "ANN")
+      click_button("Search")
 
-        expect(page).to have_content("Annabelle")
-        expect(page).to have_content("Ann")
-        expect(page).to have_content("Barbara Ann")
-        expect(page).to_not have_content("Soup")
+      expect(page).to have_content("Annabelle")
+      expect(page).to have_content("Ann")
+      expect(page).to have_content("Barbara Ann")
+      expect(page).to_not have_content("Soup")
     end
   end
 end
