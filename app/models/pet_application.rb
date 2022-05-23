e61834b4c77d20a516244efd65517aef965a503c
Casey Fazio <98674727+casefaz@users.noreply.github.com>
2022-05-23 17:42:59 -0600

    Refactor: add validation testing for pet applications

--- app/models/pet_application.rb
+++ app/models/pet_application.rb
@@ -1,4 +1,7 @@
 class PetApplication < ApplicationRecord
     belongs_to :pet
     belongs_to :application
+
+    validates_presence_of :pet_id
+    validates_presence_of :application_id
 end
--- spec/models/pet_application_spec.rb
+++ spec/models/pet_application_spec.rb
@@ -1,6 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PetApplication, type: :model do
-  it { should belong_to :pet }
-  it { should belong_to :application }
+  describe 'relationships' do 
+    it { should belong_to :pet }
+    it { should belong_to :application }
+  end 
+
+  describe 'validations' do 
+    it { should validate_presence_of(:application_id) }
+    it { should validate_presence_of(:pet_id) }
+  end
 end
